4ab3949061db1402042ecfff81dcb7671a4f0190
SocaLatchman <r.latchman3624@gmail.com>
2025-12-30 23:01:09 -0800

    Routes for project

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, redirect, url_for
+from sqlmodel import SQLModel, Session, Field, create_engine
+from flask_wtf import CSRFProtect
+from email_validator import validate_email, EmailNotValidError
+from dotenv import load_dotenv 
+from datetime import datetime
+from flask_mailman import EmailMultiAlternatives, Mail
+from typing import List, Optional
+import os
+
+load_dotenv('.env')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS')
+app.config['MAIL_RECIPIENT'] = os.environ.get('MAIL_RECIPIENT')
+csrf = CSRFProtect(app)
+mail = Mail(app)
+db_engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
+
+
+@app.route('/')
+def home():
+    pass
+
+@app.route('/about')
+def about():
+    pass
+
+@app.route('/contact')
+def contact():
+    pass
+
+@app.route('/blog')
+def blog():
+    pass
+
+@app.route('/blog/post/<post_id>')
+def blog_post():
+    pass
+
+@app.route('/projects')
+def projects():
+    pass
+
+@app.route('/dashboard/signin')
+def signin():
+    pass
+
+@app.route('/dashboard')
+def dashboard():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
