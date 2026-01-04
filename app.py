372a7d15ce8c5ad888c4d0d81f0e45bd7cf0b108
SocaLatchman <r.latchman3624@gmail.com>
2026-01-04 11:45:02 -0800

    HTML for cms signin template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
-from sqlmodel import SQLModel, Session, Field, Relationship, create_engine
+from sqlmodel import SQLModel, Session, select, Field, Relationship, create_engine
 from sqlalchemy import event
+from sqlalchemy.orm import selectinload
 from flask_wtf import CSRFProtect
 from email_validator import validate_email, EmailNotValidError
 from passlib.hash import pbkdf2_sha256
 from dotenv import load_dotenv 
-from datetime import datetime
+from datetime import datetime, date
 from flask_mailman import EmailMultiAlternatives, Mail
 from typing import List, Optional
 import os
@@ -30,10 +31,10 @@ class User(SQLModel, table=True):
     user_id: int = Field(default=None, primary_key=True)
     admin: str = Field(unique=True) 
     password: str
-    last_active: datetime = Field(default=datetime.now())
+    last_active: datetime = Field(default_factory=date.today())
     image: str
-    project_id: Optional[int] = Field(default=None, foreign_key="projects.project_id", primary_key=True)
-    image_id: Optional[int] = Field(default=None, foreign_key="images.image_id", primary_key=True)
+    project_id: Optional[int] = Field(default=None, foreign_key="projects.project_id")
+    image_id: Optional[int] = Field(default=None, foreign_key="images.image_id")
 
 class Project(SQLModel, table=True):
     __tablename__ = 'projects'
@@ -44,51 +45,70 @@ class Project(SQLModel, table=True):
     role: str
     url: str
     cover: str
-    description: str
-    date_added: datetime = Field(default=datetime.now())
-    typefaces: List["Typography"] = Relationship(back_populates='typeface')
-    colors: List['Color'] = Relationship(back_populates='color_palette')
-    logo: Optional['Logo'] = Relationship(back_populates='project_logo')
+    date_added: datetime = Field(default_factory=date.today())
+    typefaces: List['Typography'] = Relationship(back_populates='project')
+    colors: List['Color'] = Relationship(back_populates='project')
+    logo: Optional['Logo'] = Relationship(back_populates='project')
+    tags: List['Tag'] = Relationship(back_populates='project')
+
+class Tag(SQLModel, table=True):
+    __tablename__ = 'tags'
+    tag_id: int = Field(default=None, primary_key=True)
+    tag_name: str
+    project_id: Optional[int] = Field(default=None, foreign_key='projects.project_id')
+    project: Optional[Project] = Relationship(back_populates='tags')
 
 class Typography(SQLModel, table=True):
     typography_id: int = Field(default=None, primary_key=True)
     font: str
     category: str
     weight: str
-    typeface: Optional[Project] = Relationship(back_populates='typefaces')
+    project_id: Optional[int] = Field(default=None, foreign_key='projects.project_id')
+    project: Optional[Project] = Relationship(back_populates='typefaces')
 
 class Color(SQLModel, table=True):
     color_id: str = Field(default=None, primary_key=True)
     color: str
     hex_value: str
     role: str #button, background, accent color etc.
-    color_palette: Optional[Project] = Relationship(back_populates='colors')
+    project_id: Optional[int] = Field(default=None, foreign_key='projects.project_id')
+    project: Optional[Project] = Relationship(back_populates='colors')
 
 class Logo(SQLModel, table=True):
     logo_id: int = Field(default=None, primary_key=True)
     url: str
-    project_logo: Optional[Project] = Relationship(back_populates='logo')
+    project_id: Optional[int] = Field(default=None, foreign_key='projects.project_id')
+    project: Optional[Project] = Relationship(back_populates='logo')
 
 class Image(SQLModel, table=True):
     __tablename__ = 'images'
     image_id: int = Field(default=None, primary_key=True)
     url: str
-    date_added: datetime = Field(default=datetime.now())
+    date_added: datetime = Field(default_factory=date.today())
 
 class Portfolio:
-    def get_portfolio(session: Session):
-        statement = select(
-            User,
-            Project,
-            Image
-        ).join(Project).join(Image)
-        results = session.exec(statement).all()
-        for user, project, image in results:
-            return {
-                'user' : user.dict(), 
-                'project' : project.dict(), 
-                'image' : image.dict()
-            }
+    def get_portfolio(db_engine):
+        try:
+            with Session(db_engine) as session:
+                statement = select(Project).options(
+                selectinload(Project.typefaces),
+                selectinload(Project.colors),
+                selectinload(Project.logo),
+                selectinload(Project.tags)
+                )
+                results = session.exec(statement).all()
+                portfolio = []
+                for project in results:
+                    portfolio.append({
+                        'project' : project.model_dump(),
+                        'typography' : [typography.model_dump() for typography in project.typefaces],
+                        'colors' : [color.model_dump() for color in project.colors],
+                        'tag' : [tag.model_dump() for tag in project.tags],
+                        'logo' : project.logo.model_dump() if project.logo else None
+                    })
+                return portfolio
+        except Exception as e:
+            return f'Unable to connect: {e}', 404
 
 #enforce foreign keys when you use the engine(or session is created from db_engine)
 def enforce_foreign_keys(dbapi_connection, connection_record):
@@ -103,6 +123,7 @@ def create_db_and_tables():
 
 @app.route('/')
 def home():
+    portfolio = Portfolio.get_portfolio(db_engine)
     return render_template('index.html', title='Portfolio of Soca Latchman')
 
 @app.route('/about')
@@ -129,12 +150,12 @@ def blog_post():
 def projects():
     pass
 
-@app.route('/dashboard/signin')
+@app.route('/cms/signin')
 def signin():
     pass
 
-@app.route('/dashboard')
-def dashboard():
+@app.route('/cms')
+def cms():
     pass
 
 
